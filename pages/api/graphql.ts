3ccf0daf08ba0723d4f78bce4d86f24ebffb8e21
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-02-11 21:41:31 +0100

    fix(api): enable introspection to suppress router warning

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,7 +4,11 @@ import type { NextApiRequest } from 'next';
 import { typeDefs } from '../../src/schema';
 import { resolvers } from '../../src/resolvers';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
 
 export default startServerAndCreateNextHandler<NextApiRequest>(server, {
   context: async (req, res) => {
